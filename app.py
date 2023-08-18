f2e6e71c47723eda4772a22d97da3b80a0f3a11d
tiagofassoni <tiagofassoni@users.noreply.github.com>
2023-08-18 00:31:54 +0000

    Finally it is working on Replit

--- app.py
+++ app.py
@@ -8,30 +8,40 @@ app.config["SECRET_KEY"] = "fdfgkjtjkkg45yfdb"
 @app.route("/")
 def homepage():
 
-    profile = Profile()
-    profile_contents = profile.convert()
+  profile = Profile()
+  profile_contents = profile.convert()
 
-    return render_template("index.html", **profile_contents)
+  return render_template("index.html", **profile_contents)
+
 
 @app.route("/search")
 def showresults():
 
-    q = request.args.get("searchbar")
+  q = request.args.get("searchbar")
 
-    search = Search(q)
-    search_results = search.show()
+  search = Search(q)
+  search_results = search.show()
 
-    return jsonify(search_results)
+  return jsonify(search_results)
+
 
 @app.route("/results/<artist_id>", methods=['GET', 'POST'])
 def display_results(artist_id):
 
-    artist_details = Artist(artist_id)
+  artist_details = Artist(artist_id)
 
-    artistbio = artist_details.artistbio()
-    artistTracksUS = artist_details.artistTracksUS()
-    artistTracksBr = artist_details.artistTracksBr()
-    artistTracksIN = artist_details.artistTracksIN()
-    Albums = artist_details.Albums()
+  artistbio = artist_details.artistbio()
+  artistTracksUS = artist_details.artistTracksUS()
+  artistTracksBr = artist_details.artistTracksBr()
+  artistTracksIN = artist_details.artistTracksIN()
+  Albums = artist_details.Albums()
 
-    return render_template("artist.html", artistbio=artistbio, artistTracksUS=artistTracksUS, artistTracksBr=artistTracksBr, artistTracksIN=artistTracksIN, Albums=Albums)
+  return render_template("artist.html",
+                         artistbio=artistbio,
+                         artistTracksUS=artistTracksUS,
+                         artistTracksBr=artistTracksBr,
+                         artistTracksIN=artistTracksIN,
+                         Albums=Albums)
+
+
+app.run(host='0.0.0.0', port=81, debug=True)
