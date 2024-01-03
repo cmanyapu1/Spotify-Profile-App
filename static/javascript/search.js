d44b259aecd03bfc9eb6dd1d90734ebe71e4de1c
Tiago Fassoni <tiagofassoni@gmail.com>
2024-01-02 21:39:54 -0300

    Too many changes for a short commit message:
    - Fixed broken HTML code as shown by W3C Code Validator
    - Fixed Formatting of HTML and JS
    - Fixed path of Static Files
    - Added jQuery and Axios dependencies to index.html
    - Added a try/catch in search.js so failures get shown
    
    What's left:
    - The flask server returns an Error 500 when searching. Gotta investigate that.

--- static/javascript/search.js
+++ static/javascript/search.js
@@ -1,32 +1,33 @@
 const $searchbar = $("#searchbar");
 const $giflist = $("#results_list");
-const url = 'http://127.0.0.1:5000/'
+const url = "http://127.0.0.1:5000/";
 
-$("#search_form").on("submit", async function(e) {
-    console.log("HEEEEEY");
+$("#search_form").on("submit", async function (e) {
+  console.log("HEEEEEY");
 
-    e.preventDefault();
- 
-   const response = await axios.get(`/search?q=${$searchbar}`) 
-  
-    Appendhtml(response.data);
+  e.preventDefault();
+  try {
+    const response = await axios.get(`/search?q=${$searchbar}`);
+  } catch (error) {
+    console.log(error);
+  }
+
+  Appendhtml(response.data);
 });
 
 function Appendhtml(dataresponse) {
-
-    for (let y = 0; y < dataresponse.length; y++) {
-        $("#results_list").append(
-            `<li class="artistchosen" data-artist-id=${dataresponse[y].id}> ${dataresponse[y].name} </li>
-          `);
-
+  for (let y = 0; y < dataresponse.length; y++) {
+    $("#results_list").append(
+      `<li class="artistchosen" data-artist-id=${dataresponse[y].id}> ${dataresponse[y].name} </li>
+          `
+    );
   }
 }
 async function GetArtistDetails(evt) {
-    const toggleclick = $(evt.target)
-    const targetartist = toggleclick.closest("li").attr("data-artist-id") 
+  const toggleclick = $(evt.target);
+  const targetartist = toggleclick.closest("li").attr("data-artist-id");
 
-    await axios.get(`/results/${targetartist}`)
-
+  await axios.get(`/results/${targetartist}`);
 }
 
-$(".artistchosen").on("click", ".artistchosen", GetArtistDetails) 
+$(".artistchosen").on("click", ".artistchosen", GetArtistDetails);
